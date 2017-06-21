62c0276eaea9724f7a7fd01db2ca1a0c03d28d43
Ömer Buğra Selvi <omerbugra97@hotmail.com>
2017-06-22 02:37:25 +0300

    added image of car

--- src/Cars.java
+++ src/Cars.java
@@ -1,10 +1,15 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Cars {
 	private Rectangle car;
 	private int speed,x,y,w,h;
+	private BufferedImage image;
 	
 	Cars(int x,int y,int w,int h,int speed){
 		this.x=x;
@@ -12,11 +17,25 @@ public class Cars {
 		this.w=w;
 		this.h=h;
 		this.speed=speed;
+		if(speed>0){
+			changeSprite("/car_right.png");
+		}else{
+			changeSprite("/car_left.png");
+		}
 		car=new Rectangle(x,y,w,h);
+	}
+	public void changeSprite(String sprite){
+		try {
+			image= ImageIO.read(getClass().getResourceAsStream(sprite));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 	public void render(Graphics g){
 		Graphics2D g2d= (Graphics2D)g;
-		g2d.fill(car);
+		g2d.drawImage(image, car.x, car.y, null);
+		//g2d.fill(car);
 		move();
 		if(isOut()){
 			if(car.x>500)
