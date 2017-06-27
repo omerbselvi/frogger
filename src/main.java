6458d84b16e913dac309a330d3d5c884ea6adf10
Ömer Buğra Selvi <omerbugra97@hotmail.com>
2017-06-27 22:14:10 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -7,7 +7,7 @@ public class main {
 		Display game= new Display();	
 		frame.add(game);
 		frame.setSize(600,480);
-        frame.setResizable(false);
+      		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
